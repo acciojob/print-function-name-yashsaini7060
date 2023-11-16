03465703f303824370a8e57713faa945ca909a17
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 20:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function functionName(){
+
+alert(arguments.callee.name);
+
+
+}
+function AccioJob() {
+	alert(arguments.callee.name);
+}
+functionName();
+AccioJob();
